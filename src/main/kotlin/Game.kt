73622cefa269f8644b7a0be389586d48a9163a2f
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-03-21 21:56:41 +0900

    fix

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,7 +1,6 @@
 class Game {
     fun play(player1: Throw, player2: Throw): GameResult {
-        val result = player1.id - player2.id
-        when (result) {
+        when (player1.id - player2.id) {
             -1, 2 -> return GameResult.P1_WINS
             -2, 1 -> return GameResult.P2_WINS
             else -> return GameResult.TIE
